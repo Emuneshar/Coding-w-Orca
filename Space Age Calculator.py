80ce4dc65b9aee8dbd836765e646a73c926fe5b8
Eshwar Muneshar <Emuneshar@gmail.com>
2023-07-25 17:28:37 -0400

    created a date out of the input

--- Space Age Calculator.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-
-year = int(input("What year were you born?"))
-month = int(input("What month were you born in?"))
-day = int(input("What day were you born?"))
-
--- SpaceAgeCalculator.py
+++ SpaceAgeCalculator.py
@@ -4,3 +4,7 @@ year = int(input("What year were you born?"))
 month = int(input("What month were you born in?"))
 day = int(input("What day were you born?"))
 
+birthday = date(year, month, day)
+
+
+
